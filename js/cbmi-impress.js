1915af773e515e92728c48121a368303f76153e8
Charity Hilton <cahilton@regenstrief.org>
2015-02-26 11:57:42 -0500

    base commit

---
+++ js/cbmi-impress.js
@@ -0,0 +1,24 @@
+var defaultTime = 5000;
+var impress = impress();
+
+
+
+
+$(document).ready(function(){
+
+    var winWidth = $(window).outerWidth();
+    $('.step').css("width", winWidth);
+
+    // finally init impress
+    impress.init();
+
+
+    var timing;
+    document.addEventListener('impress:stepenter', function(e){
+        if (typeof timing !== 'undefined') clearInterval(timing);
+        // use the set duration or fallback to default
+        var duration = (e.target.getAttribute('data-transition-duration') ? e.target.getAttribute('data-transition-duration') : defaultTime);
+        timing = setInterval(impress.next, duration);
+    });
+});
+
